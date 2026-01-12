4f8ec1c431359dbff32f99f57e466054b321032c
haurova <radiofobia@list.ru>
2026-01-12 23:12:28 +0000

    added simple menu navigation and adding habits to the list; added validations of null values for both menu navigation and habits

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace HabitTracker
         static void Main(string[] args)
         {
 
+            List<string> habits = new List<string>();
+
             Console.WriteLine("Welcome to Habit Tracker!");
             Console.WriteLine("==========================");
             Console.WriteLine();
@@ -15,11 +17,52 @@ namespace HabitTracker
             Console.WriteLine("2. View Habits");
             Console.WriteLine("3. Exit");
             Console.WriteLine("Choose an option: ");
-            // Start by creating a simple menu system
 
-
-            int choice = int.Parse(Console.ReadLine());
+            if (!int.TryParse(Console.ReadLine(), out int choice))
+            {
+                Console.WriteLine("Choice must be a number");
+                return;
+            }
+            
             Console.WriteLine("You chose: " + choice);
+
+            if (choice == 1)
+            {
+                Console.WriteLine("You chose to add a habit");
+                string? habitName = string.Empty;
+
+                while (habitName != "exit")
+                {
+                    Console.WriteLine("Enter the name of the habit (or 'exit' to finish): ");
+                    habitName = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(habitName))
+                    {
+                        habits.Add(habitName);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Habit name cannot be empty");
+                    }
+                }
+                habits.Remove("exit");
+                Console.WriteLine("Habits " + string.Join(", ", habits) + " added successfully");
+
+
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("You chose to view habits");
+                Console.WriteLine("Habits: " + string.Join(", ", habits));
+                Console.WriteLine("Total habits: " + habits.Count);
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("You chose to exit");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice");
+            }
         }
     }
 }
